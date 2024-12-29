56d6f2bce3a57821a61612deab20b407c5ac552a
France LY <fly@reseau.free.fr>
2024-12-29 17:47:54 +0100

    ajout unit_test reponse api

--- test/unit_test.py
+++ test/unit_test.py
@@ -1,11 +1,16 @@
-# import requests
 import pickle
 import pandas as pd
 import pytest
 import numpy as np
+import requests
+from fastapi.testclient import TestClient
+
+from LY_France_API_112024 import app
+
+URL = "http://127.0.0.1:8000/"
+client = TestClient(app)
 
 model = pickle.load(open("model_LGBM.pkl", "rb"))
-
 df = pd.read_csv("test/data/data_sample.csv")
 df = df[df['TARGET'].notnull()]
 feats = [f for f in df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
@@ -16,3 +21,8 @@ def test_value_prediction_model(model = model, data = data_to_test):
     prediction = model.predict(data)
     expected_array = np.array([1.0, 0.0])
     assert (prediction == expected_array).all()
+
+def test_response_api():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Credit Scoring API"}
